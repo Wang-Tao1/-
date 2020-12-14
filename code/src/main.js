01c677034aada59f737a9ab120210e71e4087d0d
汪涛 <24189594@qq.com>
2020-12-14 19:54:54 +0800

    完成商品功能开发

--- code/src/main.js
+++ code/src/main.js
@@ -7,6 +7,11 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+
+import 'quill/dist/quill.core.css' 
+import 'quill/dist/quill.snow.css' 
+import 'quill/dist/quill.bubble.css'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config=>{
@@ -15,10 +20,20 @@ axios.interceptors.request.use(config=>{
 })
 
 Vue.prototype.$http = axios
-
+Vue.filter('dateFormat',function(originVal){
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth()+1+'').padStart(2,'0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 Vue.component('tree-table', TreeTable)
 Vue.config.productionTip = false
+Vue.use(VueQuillEditor)
 
 new Vue({
   router,
